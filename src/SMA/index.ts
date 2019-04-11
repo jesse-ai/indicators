ec25d000a5d28a0abeb4775b80697faa05e1c87d
Sully Fischer <fischersully@gmail.com>
2019-04-11 18:44:17 +0430

    update

--- src/SMA/index.ts
+++ src/SMA/index.ts
@@ -2,19 +2,19 @@
  * Simple Moving Average (SMA)
  *
  * @export
- * @param {number[]} prices
+ * @param {number[]} values
  * @param {number} period
  * @returns {number}
  */
-export default function SMA(prices: number[], period: number): number {
-    if (prices.length !== period) {
-        throw new Error('Number of values must be the same as the period.')
+export default function SMA(values: number[], period: number): number {
+    if (values.length !== period) {
+        throw new Error(`Number of values(${values.length}) must be the same as the period(${period}).`)
     }
 
     let sum: number = 0
 
-    for (let index = 0; index < prices.length; index++) {
-        sum += prices[index]
+    for (let index = 0; index < values.length; index++) {
+        sum += values[index]
     }
 
     return sum / period
